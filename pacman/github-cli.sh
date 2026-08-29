a874f72b188cf634a91968d658e952e97413882e
Joseph King <joeckr@icloud.com>
2026-08-29 10:28:04 -0400

    feat: git config auto push remote

--- pacman/github-cli.sh
+++ pacman/github-cli.sh
@@ -2,5 +2,6 @@ sudo pacman -S --noconfirm --needed github-cli
 
 git config --global user.email "joeckr@icloud.com"
 git config --global user.name "Joseph King"
+git config --global push.autoSetupRemote true   
 
 gh auth login
