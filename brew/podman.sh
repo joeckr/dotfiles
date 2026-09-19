2d8407e2a9e2083369c79f5ccc5826a5c931211e
Joseph King <joeckr@icloud.com>
2026-09-19 11:56:05 -0400

    fix: actually apply podman oc compliance out of box

--- brew/podman.sh
+++ brew/podman.sh
@@ -11,4 +11,7 @@ cp "$DOTFILES_DIR/containers/containers.conf" "$HOME/.config/containers/containe
 if ! podman machine inspect >/dev/null 2>&1; then
 	podman machine init
 	podman machine start
+	podman machine ssh "ln -sf /etc/containers/containers.conf ~/.config/containers/containers.conf && systemctl --user restart podman.socket podman.service"
+elif podman machine inspect --format '{{.State}}' 2>/dev/null | grep -q "running"; then
+	podman machine ssh "ln -sf /etc/containers/containers.conf ~/.config/containers/containers.conf && systemctl --user restart podman.socket podman.service"
 fi
